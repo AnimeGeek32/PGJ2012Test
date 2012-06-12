6300d29d6f8bc1c3f8011129b1c970fa2dbaa0e8
Daniel Jung <djanimelover18@gmail.com>
2012-06-12 00:10:24 -0400

    First Test Commit
    This is the first commit of Space Lords' PGJ 2012 Unity on Github.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Check the left and right arrow keys
+		if(Input.GetAxis("Horizontal") < 0)
+		{
+			// Then move to the left
+			//Debug.Log("Going left.");
+			transform.Translate(new Vector3(-2, 0, 0) * Time.deltaTime);
+		}
+		else if(Input.GetAxis("Horizontal") > 0)
+		{
+			// Then move to the right
+			//Debug.Log("Going right.");
+			transform.Translate(new Vector3(2, 0, 0) * Time.deltaTime);
+		}
+	}
+}
